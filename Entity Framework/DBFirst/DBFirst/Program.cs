9fe6cde3bca32e9d5531e95da73876321087373d
David Mosyan <dmosyan@gmail.com>
2019-10-17 13:14:33 +0400

    Add files via upload
    Adding and Using Stored Procedures as Methods

---
+++ Entity Framework/DBFirst/DBFirst/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.Core.Objects;
+
+namespace DBFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlutoContext DBContext = new PlutoContext();
+            ObjectResult<GetCourses_Result> courses = DBContext.GetCourses();
+            foreach (var item in courses)
+            {
+                Console.WriteLine(item.Title);
+            }
+        }
+    }
+}
